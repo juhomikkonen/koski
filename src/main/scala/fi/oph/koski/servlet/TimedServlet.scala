406b5d43e2486b7612f8e13fe6ba1ac8b6625e16
Matias Katajamäki <mkataja@reaktor.fi>
2020-06-03 21:48:52 +0300

    Kokeile ilman polkujen dekoodausta

--- src/main/scala/fi/oph/koski/servlet/TimedServlet.scala
+++ src/main/scala/fi/oph/koski/servlet/TimedServlet.scala
@@ -8,6 +8,7 @@ trait TimedServlet extends ScalatraBase with Timing {
     val blockname: String = verb + " " + request.getServletPath + path
     timed(blockname, thresholdMs = threshold)(action)
   }
+
   def get(s: String)(action: => Any) = super.get(s)(timedAction("GET", s, action))
 
   def post(s: String)(action: => Any) = super.post(s)(timedAction("POST", s, action))
@@ -15,4 +16,7 @@ trait TimedServlet extends ScalatraBase with Timing {
   def put(s: String)(action: => Any) = super.put(s)(timedAction("PUT", s, action))
 
   def delete(s: String)(action: => Any) = super.delete(s)(timedAction("DELETE", s, action))
+
+  // The default behavior breaks things since Scalatra 2.7.0
+  decodePercentEncodedPath = false
 }
