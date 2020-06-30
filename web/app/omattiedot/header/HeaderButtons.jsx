67b85780772c95eee35a0f9dbc302ef7975b1275
Matias Katajamäki <mkataja@reaktor.fi>
2020-06-30 23:41:15 +0300

    Uusi omat tiedot käli wip

--- web/app/omattiedot/header/HeaderButtons.jsx
+++ web/app/omattiedot/header/HeaderButtons.jsx
@@ -2,33 +2,10 @@ import React from 'react'
 import {MultistateToggleButton} from '../../components/ToggleButton'
 import {withFeatureFlag} from '../../components/withFeatureFlag'
 import {FormState} from './Header'
-import FloatingActionButton from '../../components/FloatingActionButton'
 import {virheRaportointiTitle} from './HeaderVirheraportointiSection'
-import {hasOpintoja} from '../../OmatTiedot'
+import {hasOpintoja} from '../../oppija/oppija'
 
 const VirheraportointiButton = withFeatureFlag(FEATURE.OMAT_TIEDOT.VIRHERAPORTOINTI, MultistateToggleButton)
-const SuoritusjakoButton = withFeatureFlag(FEATURE.OMAT_TIEDOT.SUORITUSJAKO, MultistateToggleButton)
-const FloatingSuoritusjakoButton = withFeatureFlag(FEATURE.OMAT_TIEDOT.SUORITUSJAKO, FloatingActionButton)
-
-const ACTION_BUTTON_OFFSET = 270
-const ACTION_BUTTON_ID = 'suoritusjako-button'
-
-const moveToSuoritusjako = (completionHandler) => {
-  let timer = null
-
-  const onScroll = () => {
-    if (timer !== null) clearTimeout(timer)
-
-    timer = setTimeout(() => {
-      completionHandler()
-      window.removeEventListener('scroll', onScroll)
-    }, 100)
-  }
-
-  window.addEventListener('scroll', onScroll)
-
-  document.getElementById(ACTION_BUTTON_ID).scrollIntoView({behavior: 'smooth'})
-}
 
 export const HeaderButtons = ({uiModeA, oppija}) => (
   <div className='header__buttons'>
@@ -39,18 +16,5 @@ export const HeaderButtons = ({uiModeA, oppija}) => (
       text={virheRaportointiTitle(oppija)}
       style='secondary'
     />}
-    {!oppija.context.huollettava && hasOpintoja(oppija) && <SuoritusjakoButton
-      id={ACTION_BUTTON_ID}
-      stateA={uiModeA}
-      value={FormState.SUORITUSJAKO}
-      clearedStateValue={FormState.NONE}
-      text='Jaa suoritustietoja'
-    />}
-
-    <FloatingSuoritusjakoButton
-      text='Jaa suoritustietoja'
-      onClick={() => moveToSuoritusjako(() => uiModeA.set(FormState.SUORITUSJAKO))}
-      visibilityOffset={ACTION_BUTTON_OFFSET}
-    />
   </div>
 )
